fb49e32588c98c01016858f8812e6ddc8dfd7a75
Nelson Wells <nelsonwellswku@gmail.com>
2018-03-20 23:46:33 -0500

    Demonstrate transactions working with query builder...
    but it isn't scoped to the request

--- api/src/diagnostic/handlers.js
+++ api/src/diagnostic/handlers.js
@@ -1,4 +1,6 @@
+const uuid = require('uuid/v4');
 const logger = require('../infrastructure/logger');
+const db = require('../infrastructure/database');
 
 const healthCheck = (req, res) => {
   res.json({
@@ -11,7 +13,17 @@ const error = async (req, res) => {
   throw new Error('Test Error');
 };
 
+const testTransactions = async (req, res) => {
+  await db.transaction(async (trx) => {
+    await db.queryBuilder().insert({ EmailAddress: `${uuid()}@example.com`, PasswordHash: 'not_a_real_hash' }).into('AppUser').transacting(trx);
+    await db.queryBuilder().insert({ EmailAddress: `${uuid()}@example.com`, PasswordHash: 'not_a_real_hash' }).into('AppUser').transacting(trx);
+  });
+
+  res.json({ success: true });
+};
+
 module.exports = {
   healthCheck,
   error,
+  testTransactions,
 };
--- api/src/diagnostic/index.js
+++ api/src/diagnostic/index.js
@@ -4,5 +4,6 @@ const handlers = require('./handlers');
 const router = express.Router();
 router.get('/healthCheck', handlers.healthCheck);
 router.get('/error', handlers.error);
+router.get('/transactions', handlers.testTransactions);
 
 module.exports = router;
