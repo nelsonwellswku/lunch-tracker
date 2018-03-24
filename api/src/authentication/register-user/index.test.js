5662cd93f25ad737e1f2acea207b2e8642b350c1
Nelson Wells <nelsonwellswku@gmail.com>
2018-03-24 15:23:07 -0500

    Add unit test for when a user tries to register with an email...
    that's already been registered

--- api/src/authentication/register-user/index.test.js
+++ api/src/authentication/register-user/index.test.js
@@ -8,6 +8,11 @@ jest.mock('./queries', () => mockQueries);
 
 const registerUser = require('./index');
 
+beforeEach(() => {
+  mockQueries.doesUserExist.mockClear();
+  mockQueries.createAppUser.mockClear();
+});
+
 describe('register user endpoint;', () => {
   test('can register a new user', async () => {
     const req = {
@@ -25,5 +30,32 @@ describe('register user endpoint;', () => {
 
     expect(mockQueries.createAppUser).toMatchSnapshot('create app user');
     expect(res.sendStatus).toMatchSnapshot('send status');
+  });
+
+  test('throws error if email address is already in use', async () => {
+    mockQueries.doesUserExist.mockImplementation(() => Promise.resolve(true));
+
+    const req = {
+      body: {
+        emailAddress: 'TestUsername',
+        password: 'TestPassword',
+      },
+    };
+
+    const res = {
+      sendStatus: jest.fn(),
+    };
+
+    let thrownError = null;
+    try {
+      await registerUser(req, res);
+    } catch (err) {
+      thrownError = err;
+    }
+
+    expect(thrownError).not.toBeNull();
+    expect(thrownError).toMatchSnapshot();
+    expect(mockQueries.createAppUser).not.toBeCalled();
+    expect(res.sendStatus).not.toBeCalled();
   });
 });
