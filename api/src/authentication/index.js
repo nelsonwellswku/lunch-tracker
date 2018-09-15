887d81975b402039634e9bd35920d6cb85859f17
Nelson Wells <nelsonwellswku@gmail.com>
2018-09-15 00:04:09 -0500

    Set up a registration verification page and a no-op endpoint
    that it hits on load

--- api/src/authentication/index.js
+++ api/src/authentication/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { celebrate, Joi } = require('celebrate');
 const handlers = require('./handlers');
+const errors = require('../infrastructure/errors');
 
 const router = express.Router();
 
@@ -23,5 +24,6 @@ router.post('/login', handlers.login);
 router.get('/logout', handlers.logout);
 router.post('/registerUser', celebrate(registerUserSchema, { abortEarly: false }), handlers.registerUser);
 router.post('/resetPassword', handlers.resetPassword);
+router.post('/verify-registration-token', (req, res) => { throw new errors.NotImplemented(); });
 
 module.exports = router;
