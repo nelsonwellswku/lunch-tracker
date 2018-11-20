a9212eb1d14896395c57567cc63dbfbe4daf8dee
Nelson Wells <nelsonwellswku@gmail.com>
2018-11-19 23:54:39 -0600

    Merge migrations into one file; add an undo script

--- migration/migrations/0001.do.create-initial-tables.sql
+++ migration/migrations/0001.do.create-initial-tables.sql
@@ -23,5 +23,19 @@ create table RegistrationToken
   constraint [FK_RegistrationToken_AppUser] foreign key (AppUserId) references AppUser(AppUserId)
 )
 
+create table Lunch
+(
+  LunchId int identity(1,1) not null,
+  AppUserId int not null,
+  [Location] nvarchar(60) not null,
+  Cost decimal(8,2) null,
+  Revisit nvarchar(6) not null,
+  LunchDate date not null,
+
+  constraint [PK_Lunch] primary key clustered (LunchId asc),
+  constraint [FK_Lunch_AppUser] foreign key (AppUserId) references AppUser(AppUserId)
+)
+
+
 commit transaction;
 set xact_abort off;
