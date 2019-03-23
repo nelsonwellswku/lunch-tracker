74c704489adf56bb013ec4916c3836920a7dcdd7
Nelson Wells <nelsonwellswku@gmail.com>
2019-03-23 01:11:07 -0500

    Introduce interception functions to client api for later use

--- website/src/api/client-base.ts
+++ website/src/api/client-base.ts
@@ -1,3 +1,9 @@
 export default class ClientBase {
+  async transformOptions(requestInit: RequestInit): Promise<RequestInit> {
+    return requestInit;
+  }
 
+  async transformResult(url: string, response: Response, processor: (response: Response) => any) {
+    return processor(response);
+  }
 }
