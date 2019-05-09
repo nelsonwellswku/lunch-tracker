de8a5b76507a8ce6bd00826a7d5ddabb521a5bf4
Nelson Wells <nelsonwellswku@gmail.com>
2019-05-08 22:40:34 -0500

    NSwag builds typescript client at project build time

--- api/Features/Auth/SignIn/SignInHandler.cs
+++ api/Features/Auth/SignIn/SignInHandler.cs
@@ -92,7 +92,7 @@ namespace Octogami.LunchTracker.Api.Features.Auth.SignIn
             var authProperties = new AuthenticationProperties
             {
                 AllowRefresh = true,
-                ExpiresUtc = DateTimeOffset.Now.AddMinutes(1),
+                ExpiresUtc = DateTimeOffset.Now.AddMinutes(60),
                 IsPersistent = true,
             };
 
