7f6dfa226ef4bdf97e3f2ec35e5a66d38f206809
Nelson Wells <nelsonwellswku@gmail.com>
2019-05-16 21:39:34 -0500

    Sign user out when encountering a 401 unauthorized

--- website/src/api/client-base.ts
+++ website/src/api/client-base.ts
@@ -4,17 +4,15 @@ export default class ClientBase {
   }
 
   transformOptions(options: RequestInit): Promise<RequestInit> {
-    if (this.authorizationToken) {
-      options.headers = new Headers({
-        ...options.headers,
-        'authorization': 'Bearer ' + this.authorizationToken,
-      });
-    }
-
+    // TODO: Get rid of this function,
+    // it's a leftover from using bearer tokens
     return Promise.resolve(options);
   }
 
   transformResult(url: string, response: Response, processor: (response: Response) => Promise<any>): Promise<any> {
+    if (response.status === 401) {
+      window.location.href = 'sign-out';
+    }
     return processor(response);
   }
 }
